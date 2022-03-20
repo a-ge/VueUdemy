2cb8f1f1e8012320ec81ad37c5f8702fa1cca78f
a-ge <amgesmundo@gmail.com>
2022-03-20 15:35:59 -0700

    connected buttons, calculates value, and returns result message

---
+++ basics-assignment-3-problem/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      number: 0
+    };
+  },
+  computed: {
+    result() {
+      if (this.number < 37) {
+        return 'Not there yet!';
+      } else if (this.number === 37) {
+        return this.number;
+      } else {
+        return 'Too much!'
+      }
+    }
+  },
+  methods: {
+    addNumber(num) {
+      this.number = this.number + num;
+    }
+  }
+});
+
+
+
+app.mount('#assignment');
