ac6ed573f211a9c03af5efb955ee633ede88d392
a-ge <amgesmundo@gmail.com>
2022-03-20 16:36:03 -0700

    dynamic inline styling. user can enter color

--- basics-assignment-4-problem/app.js
+++ basics-assignment-4-problem/app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       inputClass: '',
-      paragraphIsVisible: true
+      paragraphIsVisible: true,
+      inputBackgroundColor: ''
     };
   },
   computed: {
